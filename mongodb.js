e36e2b7def9fec8eecdb338ccb09f5f5641123a5
Adnan <pozegic.adnan@gmail.com>
2022-03-17 13:23:20 +0100

    Experiment with read

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,6 @@
 
 const mongodb = require( 'mongodb' );
-const { MongoClient } = mongodb;
+const { MongoClient, ObjectId } = mongodb;
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName  = 'task-manager';
@@ -13,5 +13,38 @@ MongoClient.connect( connectionURL, { useNewUrlParser: true }, ( error, client )
 	}
 
 	const db = client.db( databaseName );
+	
+	// db.collection( 'users' ).findOne( { _id: new ObjectId( '62330d88f0564fce68f8897a' ) }, ( error, user ) => {
+	// 	if ( error ) {
+	// 		console.log( 'Couldn\'t fetch user' );
+	// 		return;
+	// 	}
 
+	// 	console.log( user );
+	// });
+
+	// db.collection( 'users' ).find( { age: 31 } ).toArray( ( error, users ) => {
+	// 	console.log( users );
+	// });
+
+	// db.collection( 'users' ).find( { age: 31 } ).count( (error, users ) => {
+	// 	console.log( users );
+	// });
+
+	// db.collection( 'tasks' ).findOne( { _id: new ObjectId( '62330ee74035574be9b292dd' ) }, ( error, user ) => {
+	// 	if ( error ) {
+	// 		console.log( 'Couldnt get user' );
+	// 	}
+
+	// 	console.log( user );
+	// });
+
+	db.collection( 'tasks' ).find( { completed: false } ).toArray( (error, tasks ) => {
+		if ( error ) {
+			console.log( 'Couldn\'t initiate search' );
+			return;
+		}
+
+		console.log( tasks );
+	})
 });
