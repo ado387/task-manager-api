68ae1d575db021c72363a7e2f9c663b0c802ecb2
Adnan <pozegic.adnan@gmail.com>
2022-03-18 07:56:48 +0100

    Remove update experiment

--- mongodb.js
+++ mongodb.js
@@ -14,35 +14,5 @@ MongoClient.connect( connectionURL, { useNewUrlParser: true }, ( error, client )
 
 	const db = client.db( databaseName );
 	
-	const updatePromise = db.collection( 'users' ).updateOne(
-		{
-			_id: new ObjectId( '6233139e2f707baa3c3da7b6' )
-		},
-		{
-			$inc: {
-				age: 3
-			}
-		}
-	);
-
-	updatePromise.then( result => {
-		console.log( result );
-	}).catch( error => {
-		console.log( error );
-	});
-
-	const updateManyPromise = db.collection( 'tasks' ).updateMany(
-		{
-			completed: false,
-		},
-		{
-			$set : {
-				completed: true,
-			}
-		}
-	).then( result => {
-		console.log( result );
-	}).catch( error => {
-		console.log( error );
-	});
+	
 });
