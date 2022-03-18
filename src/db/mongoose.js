cf48c667d1edc89440e3c445025c23bb4e0110e7
Adnan <pozegic.adnan@gmail.com>
2022-03-18 11:16:56 +0100

    Tweak task and user model

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,20 +1,63 @@
-const mongoose = require( 'mongoose' );
+const mongoose  = require( 'mongoose' );
+const validator = require( 'validator' );
 
 mongoose.connect( 'mongodb://127.0.0.1:27017/task-manager-api' );
 
 const User = mongoose.model( 'User', {
 	name: {
-		type: String
+		type: String,
+		required: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		trim: true,
+		lowercase: true,
+		validate( value ) {
+			if ( ! validator.isEmail( value ) ) {
+				throw new Error( 'Email is invalid' );
+			}
+		}
+	},
+	password: {
+		type: String,
+		required: true,
+		trim: true,
+		minlength: 7,
+		validate( value ) {
+			// Use minlength dummy :)
+			// if ( value.length < 7 ) {
+			// 	throw new Error( 'Password must contain at least 7 characters' );
+			// }
+
+			if ( value.includes( 'password' ) ) {
+				throw new Error( "Password must not contains word 'password'" );
+			}
+		}
 	},
 	age: {
-		type: Number
+		type: Number,
+		default: 0,
+		validate( value ) {
+			// This is useless because value is already coerced to number by this point.
+			// if ( typeof value !== 'number' ) {
+			// 	console.log( 'typeof error');
+			// 	throw new Error( 'Age must be a number' );
+			// }
+
+			if ( value < 0 ) {
+				throw new Error( 'Age must be a positive number' );
+			}
+		}
 	}
 });
 
-// const me = new User({
-// 	name: 'Medo',
-// 	age: false, // Watch out: implicit coercion.
-// });
+const me = new User({
+	name: ' Adnan   ',
+	email: '   adnan@gmail.COM ',
+	password: '123456',
+});
 
 // me.save().then( () => {
 // 	console.log( me );
@@ -24,16 +67,18 @@ const User = mongoose.model( 'User', {
 
 const Task = mongoose.model( 'Task', {
 	description: {
-		type: String
+		type: String,
+		required: true,
+		trim: true,
 	},
 	completed: {
-		type: Boolean
+		type: Boolean,
+		default: false,
 	}
 })
 
 const newTask = new Task({
-	description: 'Finish up ClickUp tasks',
-	completed: false,
+	description: '    Finish nodejs challange  ',
 });
 
 newTask.save().then( result => {
