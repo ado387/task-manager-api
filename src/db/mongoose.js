41717fcf5a5798951707c80be5a31569d293cf77
Adnan <pozegic.adnan@gmail.com>
2022-03-18 09:04:59 +0100

    Testing creating mongoose models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,24 @@
 const mongoose = require( 'mongoose' );
 
-mongoose.connect( 'mongodb://127.0.0.1:27017/task-manager-api', {
-	useNewUrlParser: true,
-	useCreateIndex: true,
+mongoose.connect( 'mongodb://127.0.0.1:27017/task-manager-api' );
+
+const User = mongoose.model( 'User', {
+	name: {
+		type: String
+	},
+	age: {
+		type: Number
+	}
 });
+
+// const me = new User({
+// 	name: 'Medo',
+// 	age: false, // Watch out: implicit coercion.
+// });
+
+// me.save().then( () => {
+// 	console.log( me );
+// }).catch( error => {
+// 	console.log( 'Error ', error );
+// });
 
