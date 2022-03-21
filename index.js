87bb747bbeab145d00fcb058c62475366d1a04ce
Adnan <pozegic.adnan@gmail.com>
2022-03-21 12:48:01 +0100

    Add patch routes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require( 'express' );
 
 require( './src/db/mongoose' );
 
-const User = require( './src/models/user');
 const Task = require( './src/models/task' );
 
 const app  = express();
@@ -10,6 +9,11 @@ const port = process.env.PORT || 3000;
 
 app.use( express.json() );
 
+
+
+/****************************
+ * User routes
+ ****************************/
 app.get( '/users', async ( req, res ) => {
 
 	try {
@@ -53,6 +57,38 @@ app.post( '/users', async ( req, res ) => {
 	}
 });
 
+app.patch( '/users/:id', async ( req, res ) => {
+	// Validate updated fields.
+	const updates = Object.keys( req.body );
+	const allowedUpdates = [ 'name', 'email', 'password', 'age' ];
+	const isValidOperation = updates.every( update => allowedUpdates.includes( update ) );
+
+	if ( ! isValidOperation ) {
+		res.status( 400 ).send( { error: 'Invalid updates' } );
+		return;
+	}
+
+	// Try to apply updates.
+	try {
+		const user = await User.findByIdAndUpdate( req.params.id, req.body, { new: true, runValidators: true } );
+		
+		if ( ! user ) {
+			res.status( 404 ).send();
+			return;
+		}
+
+		res.send( user );
+	}
+	catch ( e ) {
+		res.status( 400 ).send();
+	}
+});
+
+
+
+/****************************
+ * Task routes
+ ****************************/
 app.get( '/tasks', async ( req, res ) => {
 
 	try {
@@ -86,20 +122,36 @@ app.post( '/tasks', async ( req, res ) => {
 
 	try {
 		const result = await task.save();
-
-		if ( ! result ) {
-			res.status( 400 ).send( result );
-			return;
-		}
-
 		res.status( 201 ).send( result );
 	}
 	catch ( e ) {
-		res.status( 500 ).send( e );
+		res.status( 400 ).send();
+	}
+});
+
+app.patch( '/tasks/:id', async ( req, res ) => {
+	const updates         = Object.keys( req.body );
+	const allowedUpdates  = [ 'description', 'completed' ];
+	const isAllowedUpdate = updates.every( update => allowedUpdates.includes( update ) );
+
+	if ( ! isAllowedUpdate ) {
+		res.send( { error: 'Invalid updates' } );
+		return;
+	}
+
+	try {
+		const task = await Task.findByIdAndUpdate( req.params.id, req.body, { new: true, runValidators: true } );
+
+		if ( ! task ) {
+			res.status( 404 ).send();
+		}
+
+		res.send( task );
+	} catch ( e ) {
+		res.status( 400 ).send();
 	}
 });
 
 app.listen( port, () => {
 	console.log( `Server is listening on port ${port}.`);
 });
-
