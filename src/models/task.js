b3393d3bbc1b4c47c378de3d2a761cc46bbcb965
Adnan <pozegic.adnan@gmail.com>
2022-03-22 14:39:13 +0100

    Add timestamps and user-task relationship

--- src/models/task.js
+++ src/models/task.js
@@ -9,16 +9,23 @@ const taskSchema = new mongoose.Schema({
 	completed: {
 		type: Boolean,
 		default: false,
+	},
+	owner: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: 'User',
 	}
+}, {
+	timestamps: true,
 });
 
-taskSchema.pre( 'save', function( next ) {
-	const task = this;
+// taskSchema.pre( 'save', function( next ) {
+// 	const task = this;
 
-	task.description = task.description.toUpperCase();
+// 	task.description = task.description.toUpperCase();
 
-	next();
-});
+// 	next();
+// });
 
 const Task = mongoose.model( 'Task', taskSchema );
 
