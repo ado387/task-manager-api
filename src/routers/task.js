4b0ef6e112cad08ba57f9edd61042230c23962fc
Adnan <pozegic.adnan@gmail.com>
2022-03-22 14:39:44 +0100

    Add auth, filters and pagination to tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,23 +1,49 @@
 const express = require( 'express' );
 const Task = require( '../models/task' );
+const auth = require( '../middleware/auth' );
 const router = new express.Router();
 
-router.get( '/tasks', async ( req, res ) => {
+// GET tasks?completed=true
+// GET tasks?limit=10&skip=0
+// GET tasks?sortBy=createdAt_desc
+router.get( '/tasks', auth, async ( req, res ) => {
+	const match = {};
+	const sort  = {};
+
+	if ( req.query.completed ) {
+		match.completed = req.query.completed === 'true' ? true : false;
+	}
+
+	if ( req.query.sortBy ) {
+		const parts = req.query.sortBy.split(':');
+		sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+		sort[parts[1]] = parts[1];
+	}
 
 	try {
-		const tasks = await Task.find( {} );
-		res.send( tasks );
+		// const tasks = await Task.find( { owner: req.user._id } );
+		await req.user.populate({
+			path: 'tasks',
+			match,
+			options: {
+				limit: parseInt( req.query.limit ),
+				skip: parseInt( req.query.skip ),
+				sort,
+			}
+		});
+		res.send( req.user.tasks );
 	}
 	catch( e ) {
 		res.status( 500 ).send();
 	}
 });
 
-router.get( '/tasks/:id', async ( req, res ) => {
+router.get( '/tasks/:id', auth, async ( req, res ) => {
 	const _id = req.params.id;
 
 	try {
-		const task = await Task.findById( _id );
+		const task = await Task.findOne( { _id, owner: req.user._id } );
+		console.log( task );
 
 		if ( ! task ) {
 			res.status( 404 ).send();
@@ -30,8 +56,12 @@ router.get( '/tasks/:id', async ( req, res ) => {
 	}
 });
 
-router.post( '/tasks', async ( req, res ) => {
-	const task = new Task( req.body );
+router.post( '/tasks', auth, async ( req, res ) => {
+	// const task = new Task( req.body );
+	const task = new Task({
+		...req.body,
+		owner: req.user._id,
+	});
 
 	try {
 		const result = await task.save();
@@ -42,7 +72,7 @@ router.post( '/tasks', async ( req, res ) => {
 	}
 });
 
-router.patch( '/tasks/:id', async ( req, res ) => {
+router.patch( '/tasks/:id', auth,  async ( req, res ) => {
 	const updates         = Object.keys( req.body );
 	const allowedUpdates  = [ 'description', 'completed' ];
 	const isAllowedUpdate = updates.every( update => allowedUpdates.includes( update ) );
@@ -53,7 +83,12 @@ router.patch( '/tasks/:id', async ( req, res ) => {
 	}
 
 	try {
-		const task = await Task.findById( req.params.id );
+		const task = await Task.findOne( { _id: req.params.id, owner: req.user._id } );
+
+		if ( ! task ) {
+			res.status( 404 ).send();
+			return;
+		}
 
 		updates.forEach( update => task[update] = req.body[update] );
 
@@ -70,14 +105,16 @@ router.patch( '/tasks/:id', async ( req, res ) => {
 	}
 });
 
-router.delete( '/tasks/:id', async ( req, res ) => {
+router.delete( '/tasks/:id', auth, async ( req, res ) => {
 	try {
-		const task = await Task.findByIdAndDelete( req.params.id );
+		const task = await Task.findOne( { _id: req.params.id, owner: req.user._id } );
 
-		if ( ! taks ) {
+		if ( ! task ) {
 			res.status( 404 ).send();
 			return;
 		}
+
+		await task.remove();
 
 		res.send( task );
 	} catch (error) {
