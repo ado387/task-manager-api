b4a0161abb33f976bd557881dbcddf0190bf16f1
Adnan <pozegic.adnan@gmail.com>
2022-03-22 07:07:30 +0100

    Save authentication token with signup and login

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 
 const express = require( 'express' );
+const { JsonWebTokenError } = require('jsonwebtoken');
 const User = require( '../models/user' );
 const router  = new express.Router();
 
@@ -33,11 +34,12 @@ router.get( '/users/:id', async ( req, res ) => {
 });
 
 router.post( '/users', async ( req, res ) => {
-	const user = new User( req.body );
-	
+	const user  = new User( req.body );
+
 	try {
-		const result = await user.save();
-		res.status( 201 ).send( result );
+		await user.save();
+		const token = await user.generateAuthToken();
+		res.status( 201 ).send( { user, token } );
 	}
 	catch( e ) {
 		res.status( 400 ).send( e );
@@ -46,8 +48,9 @@ router.post( '/users', async ( req, res ) => {
 
 router.post( '/users/login', async ( req, res ) => {
 	try {
-		const user = await User.findByCredentials( req.body.email, req.body.password );
-		res.send( user );
+		const user  = await User.findByCredentials( req.body.email, req.body.password );
+		const token = await user.generateAuthToken();
+		res.send( { user, token } );
 	} catch (error) {
 		res.status( 400 ).send();
 	}
@@ -55,8 +58,8 @@ router.post( '/users/login', async ( req, res ) => {
 
 router.patch( '/users/:id', async ( req, res ) => {
 	// Validate updated fields.
-	const updates = Object.keys( req.body );
-	const allowedUpdates = [ 'name', 'email', 'password', 'age' ];
+	const updates          = Object.keys( req.body );
+	const allowedUpdates   = [ 'name', 'email', 'password', 'age' ];
 	const isValidOperation = updates.every( update => allowedUpdates.includes( update ) );
 
 	if ( ! isValidOperation ) {
