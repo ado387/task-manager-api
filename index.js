9aa896d0c46b00966375ffd5c3ea0779ad41db4f
Adnan <pozegic.adnan@gmail.com>
2022-03-22 09:38:25 +0100

    Remove test

--- index.js
+++ index.js
@@ -11,17 +11,6 @@ const TaskRouter = require( './src/routers/task' );
 const app  = express();
 const port = process.env.PORT || 3000;
 
-// app.use( ( req, res, next ) => {
-// 	if ( req.method === 'GET' ) {
-// 		res.send( 'GET requests are disabled' );
-// 	} else {
-// 		next();
-// 	}
-// });
-
-app.use( ( req, res, next ) => {
-	res.status( 503 ).send( 'Site is under maintainance. Try again soon.' );
-});
 
 app.use( express.json() );
 app.use( UserRouter );
