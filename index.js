1705947ce330be2914de72ea3e996b6ef027e8d3
Adnan <pozegic.adnan@gmail.com>
2022-03-23 07:12:25 +0100

    Error middleware test

--- index.js
+++ index.js
@@ -12,6 +12,34 @@ const app  = express();
 const port = process.env.PORT || 3000;
 
 
+const multer = require( 'multer' );
+const upload = multer({
+	dest: 'images',
+	limits: {
+		fileSize: 1000000, // Milion bytes, 1MB
+	},
+	fileFilter( req, file, cb ) {
+		if ( ! file.originalname.match( /\.(doc|docx)$/ )) {
+			cb( new Error( 'File must be a Word document.' ) );
+			return;
+		}
+		
+		cb( undefined, true );
+	}
+});
+
+const errorMiddleware = ( req, res, next ) => {
+	throw new Error( 'Please upload the word document.' );
+	next();
+}
+
+app.post( '/upload', upload.single( 'upload' ), ( req, res ) => {
+	res.send();
+}, ( error, req, res, next ) => {
+	res.status( 400 ).send( { error: error.message } );
+});
+
+
 app.use( express.json() );
 app.use( UserRouter );
 app.use( TaskRouter );
